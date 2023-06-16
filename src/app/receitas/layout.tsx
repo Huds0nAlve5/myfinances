2fd05ae690c9710287ea2bdb1e3ea8091f685aeb
SUPF5009 <hudsonalvesfl@gmail.com>
2023-06-16 10:54:01 -0300

    ajustado a pagina de receita

--- src/app/receitas/layout.tsx
+++ src/app/receitas/layout.tsx
@@ -1,13 +1,11 @@
-import style from '../(index)/page.module.css'
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={style.body}>{children}</body>
-    </html>
+      <>
+        {children}
+      </>
   )
 }
