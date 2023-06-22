b5135fbf388dcb650723cc4d449cf81da49a984a
SUPF5009 <hudsonalvesfl@gmail.com>
2023-06-22 17:09:53 -0300

    adicionado prisma novamente

---
+++ prisma/migrations/20230622200717_migration_contas_receitas_despesas/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE `contas` (
+    `codigo` INTEGER NOT NULL AUTO_INCREMENT,
+    `conta` VARCHAR(191) NOT NULL,
+    `saldo` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`codigo`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `receitas` (
+    `codigo` INTEGER NOT NULL AUTO_INCREMENT,
+    `valor` DOUBLE NOT NULL,
+    `data_inclusao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `conta_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`codigo`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `despesas` (
+    `codigo` INTEGER NOT NULL AUTO_INCREMENT,
+    `valor` DOUBLE NOT NULL,
+    `data_inclusao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `conta_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`codigo`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `receitas` ADD CONSTRAINT `receitas_conta_id_fkey` FOREIGN KEY (`conta_id`) REFERENCES `contas`(`codigo`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `despesas` ADD CONSTRAINT `despesas_conta_id_fkey` FOREIGN KEY (`conta_id`) REFERENCES `contas`(`codigo`) ON DELETE RESTRICT ON UPDATE CASCADE;
